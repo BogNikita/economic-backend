8ac942570f645bf569a53d41568d4543fa665f8e
Nikita Bogachev <nikita.bogachyov@noorsoft.ru>
2021-12-13 18:55:47 +0500

    fix puppeter

--- controllers/NewsController.ts
+++ controllers/NewsController.ts
@@ -65,6 +65,7 @@ class NewsControleer {
       const browser = await puppeteer.launch({
         headless: true,
         defaultViewport: null,
+        args: ['--no-sandbox','--disable-setuid-sandbox']
       });
 
       const page = await browser.newPage();
